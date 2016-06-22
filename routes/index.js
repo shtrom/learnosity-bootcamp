40b343fdcc6b7c377e748482831283b605756c60
Michael Fulthorp <michael.fulthorp@learnosity.com>
2016-06-23 09:18:51 +1000

    Got the items API working in node (through the assess API).

--- routes/index.js
+++ routes/index.js
@@ -1,70 +1,32 @@
 var express = require('express');
+var uuid = require('uuid');
+
 var router = express.Router();
 
 var Learnosity = require("../lib/learnosity.dev.sdk");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var service = 'questions',
-    security = {
-        'consumer_key': 'yis0TYCu7U9V4o7M',
-        'user_id': 'demo_student'
-    },
-    secret = '74c5fd430cf1242a527f6223aebd42d30464be22',
-    request = {
-        'type': 'local_practice',
-        'state': 'initial',
-        'questions': [{
-            "response_id": "60001",
-            "type": "formula_dev",
-            "instant_feedback": true,
-            "is_math": true,
-            "stimulus": "<h4>Q1. Expand the equation \\((x + 7) ( x-3)\\)</h4>",
-            "ui_style": {
-                "type": "block-keyboard"
-            },
-            "validation": {
-                "scoring_type": "exactMatch",
-                "valid_response": {
-                    "score": 1,
-                    "value": [{
-                        "method": "equivSymbolic",
-                        "value": "x^2+4x-21",
-                        "options": {
-                            "allowDecimal": false,
-                            "inverseResult": false
-                        }
-                    }]
-                }
-            }
-        }, {
-            "response_id": "60002",
-            "type": "shorttext",
-            "case_sensitive": true,
-            "instant_feedback": true,
-            "stimulus": "<h4>Q2. What is the capital of Ireland?</h4>",
-            "validation": {
-                "scoring_type": "exactMatch",
-                "valid_response": {
-                    "score": 1,
-                    "value": "Dublin"
-                },
-                "alt_responses": [{
-                    "score": 2,
-                    "value": "Dubh Linn"
-                }, {
-                    "score": 3,
-                    "value": "Baile Átha Cliath"
-                }, {
-                    "score": 4,
-                    "value": "Baile Atha Cliath"
-                }]
-            }
-        }]
-    };
-    var assessInit = Learnosity.init(service, security, secret, request);
+    var userId = uuid.v4();
+    var sessionId = uuid.v4();
+    var initOpts = Learnosity.init(
+        'items',
+        {
+            'consumer_key': '4OkIF4wLnpI9L40m',
+            'domain': 'localhost',
+            'user_id': userId
+        },
+        '84468e36ee4d3bfea6f57fca1e2db3a5a00fa8e0',
+        {
+            'rendering_type': 'assess',
+            'session_id': sessionId,
+            'user_id': userId,
+            'activity_template_id': 'mfom_bootcamp',
+            'type': 'local_practice',
+            'state': 'initial'
+        });
 
-    res.render('index', { title: 'test', assessInit: assessInit });
+    res.render('index', { title: 'test', initOpts: JSON.stringify(initOpts) });
 });
 
 module.exports = router;
