499891cc7c8987a44ffe611406298a00feec6adc
Michael Fulthorp <michael.fulthorp@learnosity.com>
2016-06-22 11:56:20 +1000

    Added nodemon to automatically restart the server
    Signed-off-by: Michael Fulthorp <michael.fulthorp@learnosity.com>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bootcamp' });
+  res.render('index', { title: 'test' });
 });
 
 module.exports = router;
