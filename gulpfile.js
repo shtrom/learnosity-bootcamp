e2f800c7e0aa44d5de7c240972d166f80ced5092
Michael Fulthorp <michael.fulthorp@learnosity.com>
2016-06-22 16:26:37 +1000

    The build process now copies php files.

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,37 @@ const sass = require('gulp-sass');
 const minifycss = require('gulp-minify-css');
 const importCss = require('gulp-import-css');
 const rename = require('gulp-rename');
+const del = require('del');
+
+const paths = {
+    src: './src',
+    dest: './build',
+    php: ['./src/*.php'],
+    sass: './src/css/*.sass'
+};
 
 gulp.task('styles', () => {
-    return gulp.src('./public/css/*.sass')
+    return gulp.src(paths.sass)
         .pipe(sass().on('error', sass.logError))
         .pipe(rename({suffix: '.min'}))
         .pipe(importCss())
         .pipe(minifycss())
-        .pipe(gulp.dest('./build/css'));
+        .pipe(gulp.dest(paths.dest + '/css'));
+});
+
+gulp.task('php', () => {
+    return gulp.src(paths.php)
+        .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('watch', () => {
-    gulp.watch('public/css/*.sass', ['styles']);
+    gulp.watch(paths.src, ['build']);
 });
 
-gulp.task('default', ['watch'], () => {
-
+gulp.task('clean', () => {
+    return del(paths.dest);
 });
+
+gulp.task('build', ['clean', 'styles', 'php']);
+
+gulp.task('default', ['build', 'watch']);
