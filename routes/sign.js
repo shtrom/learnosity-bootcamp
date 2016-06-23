3cd5331ce901fe89683c53d581d1ffa7e652bf2c
Olivier Mehani <olivier.mehani@learnosity.com>
2016-06-23 17:15:57 +1000

    Handle different Content-types in /sign

--- routes/sign.js
+++ routes/sign.js
@@ -13,21 +13,34 @@ router.get('/', function(req, res, next) {
     res.render('sign', { title: 'Request signing', user_id: user_id, session_id: session_id });
 });
 router.post('/', function(req, res, next) {
-    b = req.body;
-    var request = JSON.parse(b.request);
-    // var request = JSON.parse('{ "user_id": 2 }');
-    // res.send(request.request);
-    var initOpts = Learnosity.init(
-        'items',					/* service */
-	{
-            'consumer_key': '4OkIF4wLnpI9L40m',		/* security */
-            'domain': 'localhost',
-            'user_id': request.user_id
-        },
-        '84468e36ee4d3bfea6f57fca1e2db3a5a00fa8e0',	/* secret */
-        request);
+    var ct = req.get('Content-type');
+    var request;
+    if (ct === 'application/json') {
+	    console.log("json: " + req.body);
+	    request = req.body;
 
-    res.json(initOpts);
+    } else if (ct === "application/x-www-form-urlencoded") {
+	    reqStr = req.body.request;
+	    var request = JSON.parse(req.body.request);
+    }
+
+    if (request !== undefined) {
+	    console.log(request.user_id);
+	    var initOpts = Learnosity.init(
+			    'items',					/* service */
+			    {
+				    'consumer_key': '4OkIF4wLnpI9L40m',	/* security */
+				    'domain': 'localhost',
+				    'user_id': request.user_id
+			    },
+			    '84468e36ee4d3bfea6f57fca1e2db3a5a00fa8e0',	/* secret */
+			    request);
+
+	    res.json(initOpts);
+
+    } else {
+	    res.send("Unknown content type for signer: " + ct);
+    }
 });
 
 module.exports = router;
