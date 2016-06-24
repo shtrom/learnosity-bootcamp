255badd578d04e75f9c49936b06cea50283b8689
Olivier Mehani <olivier.mehani@learnosity.com>
2016-06-24 14:08:25 +1000

    initOpts needs to be a JSON object... duh..
    Also, remove some logging code
    
    Signed-off-by: Olivier Mehani <olivier.mehani@learnosity.com>

--- routes/sign.js
+++ routes/sign.js
@@ -7,7 +7,6 @@ var Learnosity = require("../lib/learnosity.dev.sdk");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log(req);
     session_id = uuid.v4();
     user_id = uuid.v4();
     res.render('sign', { title: 'Request signing', user_id: user_id, session_id: session_id });
@@ -16,7 +15,6 @@ router.post('/', function(req, res, next) {
     var ct = req.get('Content-type');
     var request;
     if (ct === 'application/json') {
-	    console.log("json: " + req.body);
 	    request = req.body;
 
     } else if (ct === "application/x-www-form-urlencoded") {
@@ -25,13 +23,11 @@ router.post('/', function(req, res, next) {
     }
 
     if (request !== undefined) {
-	    console.log(request.user_id);
 	    var initOpts = Learnosity.init(
 			    'items',					/* service */
 			    {
 				    'consumer_key': '4OkIF4wLnpI9L40m',	/* security */
 				    'domain': 'localhost',
-				    'user_id': request.user_id
 			    },
 			    '84468e36ee4d3bfea6f57fca1e2db3a5a00fa8e0',	/* secret */
 			    request);
