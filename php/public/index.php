663485a7e50f9c87cf3a5325d9c186582ba76380
Michael Fulthorp <michael.fulthorp@learnosity.com>
2016-06-28 10:55:28 +1000

    Set up some routing (to make URLs nicer)

--- php/public/index.php
+++ php/public/index.php
@@ -2,25 +2,66 @@
 
 require '../src/bootstrap.php';
 
+use Learnosity\Bootcamp\Views\AddItemsView;
+use Learnosity\Bootcamp\Views\DataView;
 use Learnosity\Bootcamp\Views\MainView;
+use Learnosity\Bootcamp\Views\ReportView;
+use Learnosity\Bootcamp\Views\ScrollingView;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
+
+$infinite = function () {
+    $userId = Uuid::uuid4()->toString();
+    $sessionId = Uuid::uuid4()->toString();
+
+    $view = new ScrollingView();
+    return $view->render($userId, $sessionId);
+};
+
+$counting = function () {
+    $userId = Uuid::uuid4()->toString();
+    $sessionId = Uuid::uuid4()->toString();
+
+    $view = new MainView;
+    return $view->render($userId, $sessionId);
+};
+
+$report = function ($req) {
+    $view = new ReportView;
+    return $view->render($req);
+};
+
+$data = function ($req) {
+    $view= new DataView;
+    return $view->render($req);
+};
+
+$addItems = function ($req) {
+    $userId = Uuid::uuid4()->toString();
+    $sessionId = Uuid::uuid4()->toString();
+
+    $view = new AddItemsView();
+    return $view->render($userId, $sessionId);
+};
+
+$routes = [
+    'counting' => $counting,
+    'infinite' => $infinite,
+    'report' => $report,
+    'data' => $data,
+    'add-items' => $addItems,
+];
+
 
 $req = Request::createFromGlobals();
-$session = new Session;
-$session->start();
 
-if (!$session->has('userId')) {
-    $session->set('userId', uniqid());
+$route = substr($req->getPathInfo(), 1);
+
+if (isset($routes[$route])) {
+    $res = $routes[$route]($req);
+} else {
+    $res = $routes['counting']($req);
 }
-if (!$session->has('sessionId')) {
-    $session->set('sessionId', uniqid());
-}
 
-$userId = $session->get('userId');
-$sessionId = $session->get('sessionId');
-
-$view = new MainView;
-$res = $view->render($userId, $sessionId);
-$res->prepare($req);
-$res->send();
+$res->prepare($req)
+    ->send();
